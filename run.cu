d0ae997972bd529c85bb2db93cf5448dc52a4671
lanceberge <bergeron.lance6@gmail.com>
2021-11-26 15:05:43 -0500

    finding closest solutions, refactoring

--- run.cu
+++ run.cu
@@ -15,44 +15,56 @@ int main(int argc, char **argv)
     int NRe    = atoi(argv[1]);
     int NIm    = atoi(argv[2]);
     char *test = argv[3];
+    // the spacing on our grid, i.e. 1000 => run iteration on Nx and Ny evenly
+    // spaced points from -1000 to 1000 on x and y
+    int ReSpacing = 1000;
+    int ImSpacing = 500;
 
-    Complex *zValsInitial,   *zVals;
-    Complex *h_zValsInitial, *h_zVals;
+    // total number of points
+    int N = NRe*NIm;
+
+    Complex *zValsInitial, *h_zValsInitial;
+    Complex *zVals,        *h_zVals;
+    Complex *solns,        *h_solns;
 
     Polynomial P;
     Polynomial Pprime;
-    Complex    *solns;
-    int        *closest;
     Polynomial c_P;
     Polynomial c_Pprime;
 
+    int *closest, *h_closest;
+
+    dim3 B(16, 16, 1);
+    dim3 G((NRe + 16 - 1)/16, (NRe + 16 - 1)/16);
+
+    // arrays for initial points and points following iteration
+    cudaMalloc(&zValsInitial, N*sizeof(Complex));
+    cudaMalloc(&zVals,        N*sizeof(Complex));
+
+    // host arrays for points
+    h_zValsInitial = (Complex *)malloc(N*sizeof(Complex));
+    h_zVals        = (Complex *)malloc(N*sizeof(Complex));
+
+    h_closest = (int *)malloc(N * sizeof(int));
+    cudaMalloc(&closest, N*sizeof(int));
 
     // test on -4x^3 + 6x^2 + 2x = 0, which has roots
     // 0, ~1.78, ~-.28
     if (strcmp(test, "smallTest") == 0)
     {
-        // the spacing on our grid, i.e. 1000 => run iteration on Nx and Ny evenly
-        // spaced points from -1000 to 1000 on x and y
-        int ReSpacing = 1000;
-        int ImSpacing = 500;
-
-        // total number of points
-        int N = NRe*NIm;
-
-        // arrays for initial points and points following iteration
-        cudaMalloc(&zValsInitial, N*sizeof(Complex));
-        cudaMalloc(&zVals,        N*sizeof(Complex));
-
-        h_zValsInitial = (Complex *)malloc(NRe*NIm*sizeof(Complex));
-        h_zVals        = (Complex *)malloc(NRe*NIm*sizeof(Complex));
-
-        // create a polynomial
         int order = 3;
 
+        // arrays for solutions
+        cudaMalloc(&solns, order*sizeof(Complex));
+        h_solns = (Complex *)malloc(order * sizeof(Complex));
+
+        // create a polynomial
         P.order = order;
-        dfloat coeffs[4] = {-4, 6, 2, 0};
+        /* dfloat coeffs[4] = {-4, 6, 2, 0}; */
+        dfloat *coeffs = new dfloat[4] {-4, 6, 2, 0};
         P.coeffs = coeffs;
 
+        // it's derivative
         Pprime = derivative(P);
 
         dfloat *c_Pcoeffs;
@@ -61,41 +73,50 @@ int main(int argc, char **argv)
         cudaMalloc(&c_Pcoeffs, (order+1)*sizeof(dfloat));
         cudaMalloc(&c_Pprimecoeffs, order*sizeof(dfloat));
 
+        // make sure we have device version of our polynomials
         cudaMemcpy(c_Pcoeffs,      P.coeffs,     (order+1)*sizeof(dfloat), cudaMemcpyHostToDevice);
         cudaMemcpy(c_Pprimecoeffs, Pprime.coeffs, order*sizeof(dfloat),    cudaMemcpyHostToDevice);
 
-        c_P.order = order;
-        c_Pprime.order = order - 1;
+        c_P.order       = order;
+        c_Pprime.order  = order - 1;
         c_P.coeffs      = c_Pcoeffs;
         c_Pprime.coeffs = c_Pprimecoeffs;
 
-        int B = 256;
-        int G = N + B - 1 / B;
+        // fill our arrays with points
+        fillArrays <<< G, B >>> (ReSpacing, ImSpacing, zValsInitial, zVals, NRe, NIm);
 
-        dim3 B2(16, 16, 1);
-        dim3 G2((NRe + 16 - 1)/16, (NRe + 16 - 1)/16);
+        // then perform the newton iteration
+        newtonIterate <<< G, B >>> (zVals, c_P, c_Pprime, NRe, NIm, 100);
 
-        fillArrays      <<< G2, B2 >>> (ReSpacing, ImSpacing, zValsInitial, zVals, NRe, NIm);
-        newtonIterateV2 <<< G2, B2 >>> (zVals, c_P, c_Pprime, NRe, NIm, 1);
-
+        // copy results back to host
         cudaMemcpy(h_zValsInitial, zValsInitial, N*sizeof(Complex), cudaMemcpyDeviceToHost);
         cudaMemcpy(h_zVals,        zVals,        N*sizeof(Complex), cudaMemcpyDeviceToHost);
 
-        /* solns = (Complex *)malloc(order * sizeof(Complex)); */
+        // find the solutions to this polynomial - the unique points in zVals
+        int nSolns = findSolns(h_solns, h_zVals, order, N);
 
-        // find the solutions to this polynomial
-        /* int nSolns = findSolns(solns, h_zVals, order, N); */
+        // copy to device
+        cudaMemcpy(solns, h_solns, nSolns*sizeof(Complex), cudaMemcpyHostToDevice);
 
-        closest = (int *)malloc(N * sizeof(int));
-        outputToCSV("data.csv", N, h_zVals, closest);
-        /* outputSolnsToCSV("solns.csv", nSolns, solns); */
+        // fill *closest with an integer corresponding to the solution its closest to
+        // i.e. 0 for if this point is closest to solns[0]
+        findClosestSoln <<< G, B >>> (closest, zVals, NRe, NIm, solns, nSolns);
+
+        // copy results back to host
+        cudaMemcpy(h_closest, closest, N*sizeof(int), cudaMemcpyDeviceToHost);
+
+        // output data and solutions to csvs
+        outputToCSV("data.csv", N, h_zVals, h_closest);
+        outputSolnsToCSV("solns.csv", nSolns, h_solns);
     }
 
     // free memory
-    cudaFree(zVals)   ; cudaFree(zValsInitial) ;
-    free(h_zVals)     ; free(h_zValsInitial)   ;
-    cudaFree(c_P.coeffs); cudaFree(c_Pprime.coeffs);
-    /* free(solns)    ; free(closest)         ; */
+    cudaFree(zVals)          ; free(h_zVals)           ;
+    cudaFree(zValsInitial)   ; free(h_zValsInitial);
+    cudaFree(c_P.coeffs)     ; free(P.coeffs)          ;
+    cudaFree(c_Pprime.coeffs); free(Pprime.coeffs)     ;
+    cudaFree(closest)        ; free(h_closest)         ;
+    cudaFree(solns)          ; free(h_solns)           ;
 
     return 0;
 }
