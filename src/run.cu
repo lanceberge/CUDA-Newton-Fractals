c6e41604850f5e971399251fc20b3b917ac8e448
lanceberge <bergeron.lance6@gmail.com>
2021-11-26 19:21:01 -0500

    function to perform iteration, order 7 random polynomial setup

--- src/run.cu
+++ src/run.cu
@@ -1,4 +1,8 @@
 #include "newton.h"
+#include <string>
+
+void performIteration(Polynomial P, int NRe, int NIm, int ReSpacing, int ImSpacing,
+                      std::string filename, int Nits);
 
 int main(int argc, char **argv)
 {
@@ -15,19 +19,65 @@ int main(int argc, char **argv)
     int NIm    = atoi(argv[2]);
     char *test = argv[3];
 
+    Polynomial P;
+
+    // test on -4x^3 + 6x^2 + 2x = 0, which has roots
+    // 0, ~1.78, ~-.28
+    if (strcmp(test, "smallTest") == 0)
+    {
+        int order = 3;
+
+        // create a polynomial
+        P.order = order;
+
+        dfloat *coeffs = new dfloat[4] {-4, 6, 2, 0};
+        P.coeffs = coeffs;
+
+        // it's derivative
+        Polynomial Pprime = derivative(P);
+
+        // the spacing on our grid, i.e. 1000 => run iteration on Nx and Ny evenly
+        // spaced points from -1000 to 1000 on x and y
+        dfloat ReSpacing = 4;
+        dfloat ImSpacing = 4;
+
+        int Nits = 100;
+
+        performIteration(P, NRe, NIm, ReSpacing, ImSpacing,
+                         "smallTest", Nits);
+    }
+
+    else if (strcmp(test, "bigTest") == 0)
+    {
+        srand48(123456);
+
+        int order = 7;
+        dfloat *coeffs = (dfloat *)malloc((order + 1)*sizeof(dfloat));
+
+        P.order = order;
+
+        for (int i = 0; i < order + 1; ++i)
+        {
+            coeffs[i] = -10 + 20*(drand48());
+        }
+
+        P.coeffs = coeffs;
+    }
+
+    return 0;
+}
+
+void performIteration(Polynomial P, int NRe, int NIm, int ReSpacing, int ImSpacing,
+                      std::string filename, int Nits)
+{
     // total number of points
     int N = NRe*NIm;
 
-    Complex *zValsInitial, *h_zValsInitial;
-    Complex *zVals,        *h_zVals;
-    Complex *solns,        *h_solns;
+    Complex *zValsInitial;
+    Complex *zVals;
+    Complex *solns;
 
-    Polynomial P;
-    Polynomial Pprime;
-    Polynomial c_P;
-    Polynomial c_Pprime;
-
-    int *closest, *h_closest;
+    int *closest;
 
     dim3 B(16, 16, 1);
     dim3 G((NRe + 16 - 1)/16, (NRe + 16 - 1)/16);
@@ -37,67 +87,54 @@ int main(int argc, char **argv)
     cudaMalloc(&zVals,        N*sizeof(Complex));
 
     // host arrays for points
-    h_zValsInitial = (Complex *)malloc(N*sizeof(Complex));
-    h_zVals        = (Complex *)malloc(N*sizeof(Complex));
+    Complex *h_zValsInitial = (Complex *)malloc(N*sizeof(Complex));
+    Complex *h_zVals        = (Complex *)malloc(N*sizeof(Complex));
 
-    h_closest = (int *)malloc(N * sizeof(int));
+    int *h_closest = (int *)malloc(N * sizeof(int));
     cudaMalloc(&closest, N*sizeof(int));
 
-    // test on -4x^3 + 6x^2 + 2x = 0, which has roots
-    // 0, ~1.78, ~-.28
-    if (strcmp(test, "smallTest") == 0)
-    {
-        int order = 3;
+    // it's derivative
+    Polynomial Pprime = derivative(P);
 
-        // the spacing on our grid, i.e. 1000 => run iteration on Nx and Ny evenly
-        // spaced points from -1000 to 1000 on x and y
-        dfloat ReSpacing = 4;
-        dfloat ImSpacing = 4;
+    // device versions for newtonIterate
+    Polynomial c_P      = deviceP(P);
+    Polynomial c_Pprime = deviceP(Pprime);
 
-        // arrays for solutions
-        cudaMalloc(&solns, order*sizeof(Complex));
-        h_solns = (Complex *)malloc(order * sizeof(Complex));
+    // arrays for solutions
+    int order = P.order;
 
-        // create a polynomial
-        P.order = order;
-        /* dfloat coeffs[4] = {-4, 6, 2, 0}; */
-        dfloat *coeffs = new dfloat[4] {-4, 6, 2, 0};
-        P.coeffs = coeffs;
+    cudaMalloc(&solns, order*sizeof(Complex));
+    Complex *h_solns = (Complex *)malloc(order * sizeof(Complex));
 
-        // it's derivative
-        Pprime = derivative(P);
+    // fill our arrays with points
+    fillArrays <<< G, B >>> (ReSpacing, ImSpacing, zValsInitial, zVals, NRe, NIm);
 
-        // device versions for newtonIterate
-        Polynomial c_P      = deviceP(P);
-        Polynomial c_Pprime = deviceP(Pprime);
+    // then perform the newton iteration
+    newtonIterate <<< G, B >>> (zVals, c_P, c_Pprime, NRe, NIm, Nits);
 
-        // fill our arrays with points
-        fillArrays <<< G, B >>> (ReSpacing, ImSpacing, zValsInitial, zVals, NRe, NIm);
+    // copy results back to host
+    cudaMemcpy(h_zValsInitial, zValsInitial, N*sizeof(Complex), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_zVals,        zVals,        N*sizeof(Complex), cudaMemcpyDeviceToHost);
 
-        // then perform the newton iteration
-        newtonIterate <<< G, B >>> (zVals, c_P, c_Pprime, NRe, NIm, 100);
+    // find the solutions to this polynomial - the unique points in zVals
+    int nSolns = findSolns(h_solns, h_zVals, order, N);
 
-        // copy results back to host
-        cudaMemcpy(h_zValsInitial, zValsInitial, N*sizeof(Complex), cudaMemcpyDeviceToHost);
-        cudaMemcpy(h_zVals,        zVals,        N*sizeof(Complex), cudaMemcpyDeviceToHost);
+    // copy to device
+    cudaMemcpy(solns, h_solns, nSolns*sizeof(Complex), cudaMemcpyHostToDevice);
 
-        // find the solutions to this polynomial - the unique points in zVals
-        int nSolns = findSolns(h_solns, h_zVals, order, N);
+    // fill *closest with an integer corresponding to the solution its closest to
+    // i.e. 0 for if this point is closest to solns[0]
+    findClosestSoln <<< G, B >>> (closest, zVals, NRe, NIm, solns, nSolns);
 
-        // copy to device
-        cudaMemcpy(solns, h_solns, nSolns*sizeof(Complex), cudaMemcpyHostToDevice);
+    // copy results back to host
+    cudaMemcpy(h_closest, closest, N*sizeof(int), cudaMemcpyDeviceToHost);
 
-        // fill *closest with an integer corresponding to the solution its closest to
-        // i.e. 0 for if this point is closest to solns[0]
-        findClosestSoln <<< G, B >>> (closest, zVals, NRe, NIm, solns, nSolns);
+    // output data and solutions to csvs
+    std::string outputFilename = "data/"+filename+"Data.csv";
+    std::string solnFilename = "data/"+filename+"Solns.csv";
 
-        // copy results back to host
-        cudaMemcpy(h_closest, closest, N*sizeof(int), cudaMemcpyDeviceToHost);
-
-        // output data and solutions to csvs
-        outputToCSV("data/smallData.csv", N, h_zValsInitial, h_closest);
-        outputSolnsToCSV("data/smallSolns.csv", nSolns, h_solns);
-    }
+    outputToCSV(outputFilename.c_str(), N, h_zValsInitial, h_closest);
+    outputSolnsToCSV(solnFilename.c_str(), nSolns, h_solns);
 
     // free memory
     cudaFree(zVals)          ; free(h_zVals)           ;
@@ -106,6 +143,4 @@ int main(int argc, char **argv)
     cudaFree(c_Pprime.coeffs); free(Pprime.coeffs)     ;
     cudaFree(closest)        ; free(h_closest)         ;
     cudaFree(solns)          ; free(h_solns)           ;
-
-    return 0;
 }
