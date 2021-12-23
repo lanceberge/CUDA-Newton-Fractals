5353760bea3e8444dee9cccde6ced4df7c617c9a
lanceberge <bergeron.lance6@gmail.com>
2021-12-23 14:29:09 -0500

    use operator overloading for complex structs

--- src/complex.cu
+++ src/complex.cu
@@ -1,30 +1,30 @@
 #include "complex.h"
 
 // return the product of two complex numbers
-__device__ Complex cMul(Complex z1, Complex z2)
+__device__ Complex Complex::operator*(Complex z2)
 {
-    dfloat Re = z1.Re*z2.Re - z1.Im*z2.Im;
-    dfloat Im = z1.Re*z2.Im + z1.Im*z2.Re;
+    dfloat Re = this->Re*z2.Re - this->Im*z2.Im;
+    dfloat Im = this->Re*z2.Im + this->Im*z2.Re;
 
     Complex z = {Re, Im};
     return z;
 }
 
 // subtract two complex numbers
-__device__ Complex cSub(Complex z1, Complex z2)
+__device__ Complex Complex::operator-(Complex z2)
 {
-    Complex z = {z1.Re - z2.Re, z1.Im - z2.Im};
+    Complex z = {this->Re - z2.Re, this->Im - z2.Im};
     return z;
 }
 
 // divide two complex numbers - implementation from
 // https://pixel.ecn.purdue.edu:8443/purpl/WSJ/projects/DirectionalStippling/include/cuComplex.h
-__device__ Complex cDiv(Complex z1, Complex z2)
+__device__ Complex Complex::operator/(Complex z2)
 {
     dfloat s = (fabs(z2.Re)) + (fabs(z2.Im));
     dfloat oos = 1.0 / s;
-    dfloat ars = z1.Re * oos;
-    dfloat ais = z1.Im * oos;
+    dfloat ars = this->Re * oos;
+    dfloat ais = this->Im * oos;
     dfloat brs = z2.Re * oos;
     dfloat bis = z2.Im * oos;
     s = (brs * brs) + (bis * bis);
@@ -39,7 +39,7 @@ __device__ Complex cDiv(Complex z1, Complex z2)
 }
 
 // print a complex number
-void printComplex(Complex z)
+void Complex::printComplex(Complex z)
 {
     printf("Re: %f, Im: %f\n", z.Re, z.Im);
 }
