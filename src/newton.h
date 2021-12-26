8984fc51be7dc126edfb51ea4a36a9a3fcccb5c7
lanceberge <bergeron.lance6@gmail.com>
2021-12-26 14:09:49 -0500

    const references in newton

--- src/newton.h
+++ src/newton.h
@@ -8,10 +8,10 @@ __global__ void fillArrays(dfloat ReSpacing, dfloat ImSpacing, Complex *zValsIni
 
 // perform Nit iterations of newton's method on a polynomial p
 __global__ void newtonIterate(Complex *zVals, Polynomial P, Polynomial Pprime,
-                                 int NRe, int NIm, int Nit);
+                                  int NRe, int NIm, int Nit);
 
 // find all of the unique values in zVals
-int findSolns(Polynomial P, Complex *solns, Complex *zVals,
+int findSolns(const Polynomial& P, Complex *solns, Complex *zVals,
                                      int nSolns, int nVals);
 
 // for each val in zVals, find the solution in solns it's closest to
@@ -19,7 +19,7 @@ __global__ void findClosestSoln(int *closest, Complex *zVals, int NRe, int NIm,
                                    Complex *solns, int nSolns, int norm);
 
 // L2 distance between two points
-__device__ dfloat L2Distance(Complex z1, Complex z2);
+__device__ dfloat L2Distance(const Complex& z1, const Complex& z2);
 
 // L1 distance between two points
-__device__ dfloat L1Distance(Complex z1, Complex z2);
+__device__ dfloat L1Distance(const Complex& z1, const Complex& z2);
