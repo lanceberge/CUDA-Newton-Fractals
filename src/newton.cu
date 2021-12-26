ca2195c4ff54072fabb183403db08e92b70d2582
lanceberge <bergeron.lance6@gmail.com>
2021-12-26 14:09:49 -0500

    const references in newton

--- src/newton.cu
+++ src/newton.cu
@@ -59,7 +59,7 @@ __global__ void newtonIterate(Complex *zVals, Polynomial P, Polynomial Pprime,
 // nSolns - order of the polynomial
 // after running the iteration, zVals should represent n unique values corresponding
 // to the solutions of the polynomial, this function finds those unique values
-int findSolns(Polynomial P, Complex *solns, Complex *zVals,
+int findSolns(const Polynomial& P, Complex *solns, Complex *zVals,
                                    int nSolns, int nVals)
 {
     int nFound = 0;
@@ -144,7 +144,7 @@ __global__ void findClosestSoln(int *closest, Complex *zVals, int NRe, int NIm,
 }
 
 // compute the L2 distance between two points
-__device__ dfloat L2Distance(Complex z1, Complex z2)
+__device__ dfloat L2Distance(const Complex& z1, const Complex& z2)
 {
     dfloat ReDiff = z1.Re - z2.Re;
     dfloat ImDiff = z1.Im - z2.Im;
@@ -153,7 +153,7 @@ __device__ dfloat L2Distance(Complex z1, Complex z2)
 }
 
 // compute the L1 distance between two points
-__device__ dfloat L1Distance(Complex z1, Complex z2)
+__device__ dfloat L1Distance(const Complex& z1, const Complex& z2)
 {
     dfloat ReDiff = z1.Re - z2.Re;
     dfloat ImDiff = z1.Im - z2.Im;
